f1799ca17ba1f1a3cfb6c394f68dd06a388b0978
nannipy <pernazza.1936239@studenti.uniroma1.it>
2024-11-29 11:05:07 +0100

    scheda con icone

--- app/scheda/[userId]/page.tsx
+++ app/scheda/[userId]/page.tsx
@@ -4,268 +4,58 @@ import React, { useState } from 'react'
 import { Card, CardHeader, CardContent } from "@/components/ui/card"
 import { Tabs, TabsContent } from "@/components/ui/tabs"
 import { motion } from 'framer-motion';
+import { Workout, tabs, workoutSections } from '@/components/workout';
+import { Timer, Repeat } from 'lucide-react';
 
-
-const tabs = [
-  { id: "push", label: "PUSH" },
-  { id: "pull", label: "PULL" },
-  { id: "upper", label: "UPPER" },
-];
-
-interface Workout {
-  esercizio: string;
-  serie: number | string;
-  rep: number | string;
-  rest: string;
-  note: string[];
-}
+const renderWorkoutTable = (workouts: Workout[]) => (
+  <div className="w-full overflow-x-auto">
+    <div className="space-y-2">
+      {workouts.map((workout, index) => (
+        <div 
+          key={index} 
+          className="bg-neutral-900 rounded-xl p-3 shadow-lg hover:bg-neutral-800/90 transition-all duration-300 ease-in-out"
+        >
+          <div className="flex items-center justify-between mb-1">
+            <div className="flex items-center space-x-2">
+              <span className="font-bold text-sm md:text-base text-white">
+                {workout.esercizio}
+              </span>
+            </div>
+            <div className="flex items-center space-x-3">
+              <div className="flex items-center space-x-1">
+                <Repeat className="text-green-400 w-4 h-4" />
+                <span className="text-xs text-gray-300">{workout.serie}</span>
+              </div>
+              <div className="flex items-center space-x-1">
+                <p className="text-xs text-gray-300"> Reps: </p>
+                <span className="text-xs text-gray-300">{workout.rep}</span>
+              </div>
+              <div className="flex items-center space-x-1">
+                <Timer className="text-purple-400 w-4 h-4" />
+                <span className="text-xs text-gray-300">{workout.rest}</span>
+              </div>
+            </div>
+          </div>
+          {workout.note.length > 0 && (
+            <div className="pl-7 mt-1 border-t border-neutral-700 pt-1">
+              <ul className="text-xs text-gray-400 space-y-1">
+                {workout.note.map((note, noteIndex) => (
+                  <li key={noteIndex} className="flex items-start space-x-2">
+                    <span className="text-xs text-neutral-500">•</span>
+                    <span>{note}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 const WorkoutScheda = () => {
   const [activeTab, setActiveTab] = useState("push")
-
-  const workoutSections = {
-    push: [
-      {
-        esercizio: "Lento avanti con manubri",
-        serie: 3,
-        rep: 8,
-        rest: "2'-3'",
-        note: [
-          "mantieni gli avambracci perpendicolari",
-          "Lavora con il braccio a 45° col busto"
-        ]
-      },
-      {
-        esercizio: "Alzate laterali",
-        serie: 3,
-        rep: 12,
-        rest: "1'-1'30''",
-        note: [
-          "lavora con il braccio leggermente più avanti rispetto al busto"
-        ]
-      },
-      {
-        esercizio: "Spinte con manubri su panca a 30°",
-        serie: 3,
-        rep: 8,
-        rest: "2'-3'",
-        note: [
-          "mantieni avambracci perpendicolari",
-          "Gomiti a 45° col busto"
-        ]
-      },
-      {
-        esercizio: "Chest press",
-        serie: 3,
-        rep: "8/10",
-        rest: "2'-3'",
-        note: [
-          "mantieni avambracci perpendicolari",
-          "Posiziona l'impugnatura ad altezza capezzolo circa",
-          "Braccio a 45° col busto"
-        ]
-      },
-      {
-        esercizio: "Croci ai cavi",
-        serie: 2,
-        rep: 12,
-        rest: "1'-1'30''",
-        note: [
-          "puoi farle anche con i manubri"
-        ]
-      },
-      {
-        esercizio: "French press con manubri",
-        serie: 3,
-        rep: "8/10",
-        rest: "1'-2'",
-        note: [
-          "mantieni i gomiti fermi"
-        ]
-      },
-      {
-        esercizio: "Push down al cavo alto con corda",
-        serie: 3,
-        rep: "10/12",
-        rest: "1'-1'30''",
-        note: [
-          "mantieni i gomiti fermi"
-        ]
-      },
-      {
-        esercizio: "Sit up con peso",
-        serie: 3,
-        rep: 12,
-        rest: "1'",
-        note: []
-      }
-    ],
-    pull: [
-      {
-        esercizio: "Lat machine",
-        serie: 3,
-        rep: "8/10",
-        rest: "2'-3'",
-        note: [
-          "Mantieni una presa media poco più larga delle spalle",
-          "mantieni sempre gli avambracci perpendicolari"
-        ]
-      },
-      {
-        esercizio: "Pulley",
-        serie: 3,
-        rep: "8/10",
-        rest: "2'-3'",
-        note: [
-          "da eseguire con il triangolo",
-          "Porta il triangolo ad altezza ombelico",
-          "Non andare indietro con il busto nell'esecuzione"
-        ]
-      },
-      {
-        esercizio: "Pull down",
-        serie: 3,
-        rep: "10/12",
-        rest: "1'-2'",
-        note: [
-          "mantieni l'apertura dei gomiti invariata per tutto l'allenamento",
-          "Concentrati sull'avvicinare il braccio al busto",
-          "Da eseguire con una corda (la più lunga che hai in palestra)"
-        ]
-      },
-      {
-        esercizio: "Panca scott con bilanciere",
-        serie: 3,
-        rep: "8/10",
-        rest: "1'-1'30''",
-        note: []
-      },
-      {
-        esercizio: "Hammer curl su panca a 60°",
-        serie: 3,
-        rep: 10,
-        rest: "1'-1'30''",
-        note: []
-      }
-    ],
-    upper: [
-      {
-        esercizio: "Spinte con manubri con panca a 15°",
-        serie: 3,
-        rep: 8,
-        rest: "2'-3'",
-        note: [
-          "mantieni gli avambracci perpendicolari sempre",
-          "Gomiti 45° col busto"
-        ]
-      },
-      {
-        esercizio: "Panca a 45° al multipower",
-        serie: 3,
-        rep: "8/10",
-        rest: "2'-3'",
-        note: [
-          "mantieni gli avambracci perpendicolari sempre perpendicolari",
-          "Gomiti a 45° col busto"
-        ]
-      },
-      {
-        esercizio: "Shoulder press",
-        serie: 3,
-        rep: "8/10",
-        rest: "2'-3'",
-        note: [
-          "mantieni gli avambracci perpendicolari",
-          "Lavora con il braccio a 45° col busto"
-        ]
-      },
-      {
-        esercizio: "Alzate laterali singole al cavo",
-        serie: 3,
-        rep: 12,
-        rest: "1'",
-        note: [
-          "braccio leggermente avanti rispetto al busto",
-          "Posiziona il cavo ad altezza del bacino"
-        ]
-      },
-      {
-        esercizio: "Rematore con manubri su panca a 30°",
-        serie: 3,
-        rep: 8,
-        rest: "2'-3'",
-        note: [
-          "posizionati a pancia in giù sullo schienale a 30° con la testa che sporge",
-          "Porta i gomiti verso l'alto e verso dietro con un angolo di 90° in contrazione"
-        ]
-      },
-      {
-        esercizio: "Alzate posteriori con panca a 30°",
-        serie: 3,
-        rep: 15,
-        rest: "1'-1'30''",
-        note: [
-          "posizionati a pancia in giù sulla panca con la testa che sporge",
-          "Cerca di non avvicinare le scapole nell'esecuzione"
-        ]
-      },
-      {
-        esercizio: "Leg raise appeso alla sbarra",
-        serie: 3,
-        rep: "Cedimento",
-        rest: "1'",
-        note: []
-      }
-    ]
-  }
-
-  const renderWorkoutTable = (workouts: Workout[]) => (
-    <div className="w-full overflow-x-auto">
-      <table className="w-full text-white border-separate border-spacing-y-2">
-        <thead className="bg-neutral-800 text-xs md:text-sm">
-          <tr>
-            <th className="p-2 text-left rounded-l-xl w-1/3 md:w-auto">Esercizio</th>
-            <th className="p-2 text-center w-16">Serie</th>
-            <th className="p-2 text-center w-16">Rep</th>
-            <th className="p-2 text-center w-16">Rest</th>
-            <th className="p-2 text-left rounded-r-xl w-1/3 md:w-auto">Note</th>
-          </tr>
-        </thead>
-        <tbody>
-          {workouts.map((workout, index) => (
-            <tr 
-              key={index} 
-              className="bg-neutral-900 hover:bg-neutral-800/80 transition-colors duration-200 ease-in-out"
-            >
-              <td className="p-2 font-semibold text-sm md:text-base break-words">
-                {workout.esercizio}
-              </td>
-              <td className="p-2 text-center text-xs md:text-sm">
-                {workout.serie}
-              </td>
-              <td className="p-2 text-center text-xs md:text-sm">
-                {workout.rep}
-              </td>
-              <td className="p-2 text-center text-xs md:text-sm">
-                {workout.rest}
-              </td>
-              <td className="p-2 text-left">
-                {workout.note.length > 0 && (
-                  <ul className="text-xs md:text-sm space-y-1">
-                    {workout.note.map((note, noteIndex) => (
-                      <li key={noteIndex} className="list-disc list-inside">
-                        {note}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
 
   return (
     <div className="min-h-screen p-4 sm:p-6 md:p-8 lg:px-20 xl:px-40">
@@ -298,14 +88,14 @@ const WorkoutScheda = () => {
                     font-medium text-white 
                     outline-sky-400 transition 
                     focus-visible:outline-2
-                    ${activeTab === tab.id ? 'text-black' : 'hover:text-white/60'}
+                    ${activeTab === tab.id ? 'text-black' : 'hover:text-gray-400'}
                   `}
                   style={{WebkitTapHighlightColor: "transparent"}}
                 >
                   {activeTab === tab.id && (
                     <motion.span
                       layoutId="bubble"
-                      className="absolute inset-0 z-10 bg-white mix-blend-difference rounded-2xl"
+                      className="absolute inset-0 z-10 bg-white mix-blend-difference rounded-xl"
                       transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
                     />
                   )}
