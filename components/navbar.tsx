2f60a0fa81c4f324daa90b8e34ffdbd5fed7d9b6
nannipy <pernazza.1936239@studenti.uniroma1.it>
2024-11-29 02:30:31 +0100

    scheda 4

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,12 +18,12 @@ export default function Navbar() {
       {/* Navbar per tablet e desktop */}
       <nav className="hidden md:flex bg-white p-6 rounded-full m-4 mx-40">
         <div className="container mx-auto flex justify-between items-center">
-          
+        <Link href="/" className="text-black text-4xl font-bold mx-16 ">
+                ADM
+              </Link>
           <div className="space-x-4 text-xl px-10">
             <SignedIn>
-            <Link href="/" className="text-black text-4xl font-bold mx-16 ">
-                ADM sport and nutrition
-              </Link>
+           
               <Link href={`/workout/${user?.id}`} className={`text-black p-4 rounded-full hover:bg-gray-100 ${isActive(`/workout/${user?.id}`)}`}>
                 Allenamento
               </Link>
